0e2668c78b7d247369d8101977604b32301f7c5b
GianluigiCelentano <cgianluigi96@gmail.com>
2021-06-10 23:29:59 +0200

    js-campominato

--- script.js
+++ script.js
@@ -19,7 +19,7 @@
 // while userNums.length <= 84
 //     chiedi all'utente un numero 
 //     se askNums non è nell'array utentNums, ce lo aggiungo
-//         if num in array listNum (numeri generati dal computer), then break. // il gioco è finito, si stoppa il ciclo.
+//         se asknum è in array listNum (numeri generati dal computer), then break. // il gioco è finito, si stoppa il ciclo.
 //         else push num to userNums => next step.
 
 // risultato, stampo lo score che è userNums.length.
@@ -40,14 +40,27 @@ var randomNumbersPc = randomNum()
 
 var utentNums = []
 while (utentNums.length <= 84) {
-    
     var askNums = parseInt(prompt("Inserisci il tuo numero"))
-    if (parseInt(askNums) === randomNumbersPc.length) {
-        alert("GIOCO FINITO, HAI PRESO LA BOMBA")
-    }
-    else if (utentNums.includes(askNums) === false) {
+    if (utentNums.includes(askNums) === false) {
         utentNums.push(askNums)
     } else {
         alert("Non puoi inserire lo stesso numero")
     }
 }
+if (randomNumbersPc.includes(askNums) === true) {
+    alert("GIOCO FINITO, HAI PRESO LA BOMBA") 
+
+}
+    
+
+
+
+//     if (parseInt(askNums) === randomNumbersPc.length) {
+//         alert("GIOCO FINITO, HAI PRESO LA BOMBA")
+//     }
+//     else if (utentNums.includes(askNums) === false) {
+//         utentNums.push(askNums)
+//     } else {
+//         alert("Non puoi inserire lo stesso numero")
+//     }
+
